7ecb200ae6115c927e23add3a1cd5d4220169baa
thaysgama <thays.s.gama@gmail.com>
2021-12-11 17:40:31 -0300

    modifica projetos, insere curriculo, modifica botões

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,25 @@
 import './style.scss';
+import pdf from "../../assets/CV-ThaysGama-FullStack.pdf";
 
 function About(){
 
     return(
         <div className="about-container d-flex align-items-center" >
             <div id="about"></div>
-            <div className="about-text" data-aos="zoom-in-right" data-aos-duration="1500">
+            <div className="about-text" data-aos="fade-up" data-aos-duration="1500">
                 <h2>About Me</h2>
                 <p>Hi, I’m Thays. Nice to meet you! I’m a Full Stack developer based in Fortaleza, Brazil. I’ve been studying tech since 2020 focusing mostly in Java and Javascript. I am passionate about creating simple, beautiful and intuitive experiences. </p>
                 <p>Before moving into development, I worked designing buildings, as an architect. My experience help me expand my creativity, close attention to detail and team collaboration. I am currently seeking job opportunities to continue growing. Visit my LinkedIn profile for more details or just contact me.</p>
                 
-                <div className="icons d-flex align-items-center justify-content-around">
-                    <a title="linkedin" href="https://www.linkedin.com/in/thaysgama/"><i class="devicon-linkedin-plain"></i></a>
-                    <a title="github" href="https://github.com/thaysgama"><i class="devicon-github-original"></i></a>
+                <div className="icons d-flex flex-wrap align-items-center justify-content-around">
+                    <a className='d-flex align-items-center my-4' title="linkedin" href="https://www.linkedin.com/in/thaysgama/"><i class="devicon-linkedin-plain"></i>Linkedin</a>
+                    <a className='d-flex align-items-center my-4' title="github" href="https://github.com/thaysgama"><i class="devicon-github-original"></i>GitHub</a>
+                    <a className='d-flex align-items-center my-4' title="cv" href={pdf} download>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" fill="currentColor" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16">
+                            <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                        Download CV
+                    </a>
                 </div>
                 
                 
--- src/components/ProjectsGrid/index.js
+++ src/components/ProjectsGrid/index.js
@@ -4,89 +4,98 @@ import { Fragment } from 'react';
 
 function ProjectsGrid(){
     
-    const projectsData = [{
+    const projectsData = [
+      {
         id: 101,
-        name: 'Sudaca Filmes',
-        tag: 'Group Work',
-        tools: 'Html, CSS and JS',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/dh-frontend-sudaca',
-        websiteUrl: 'https://thaysgama.github.io/dh-frontend-sudaca/',
-        bg: 'gradient-1'
-    },{
-        id: 102,
-        name: 'To Do List',
-        tag: 'Group Work',
-        tools: 'Html, CSS and JS',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/front-end_II-todo-list',
-        websiteUrl: 'https://sophiafiama.github.io/front-end_II-todo-list/',
-        bg:'gradient-2'
-    }, {
-        id: 103,
-        name: 'Donation Form',
-        tag: 'Group Work',
-        tools: 'Html, CSS and JS',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/dh-frontendII-formulario',
-        websiteUrl: 'https://thaysgama.github.io/dh-frontendII-formulario/',
-        bg: 'gradient-3'
-    }, {
-        id: 104,
-        name: 'Irmão do Jorel Form',
-        tag: 'Individual Work',
-        tools: 'Html, CSS, JS and React',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/dh-frontIII-irmao-do-jorel',
-        websiteUrl: 'https://dh-front-iii-irmao-do-jorel.vercel.app/',
-        bg: 'gradient-3'
-    },{
-        id: 105,
-        name: 'T&M Vehicle Hire',
-        tag: 'Pair Work',
-        tools: 'Java',
-        website:false,
-        gitHubUrl: 'https://github.com/thaysgama/locadora-veiculos',
-        websiteUrl: '',
-        bg: 'gradient-1'
-    }, {
-        id: 106,
-        name: 'Dentist Registation',
-        tag: 'Individual Work',
-        tools: 'Java and Maven',
-        website:false,
-        gitHubUrl: 'https://github.com/thaysgama/dh-backend-checkpointI',
-        websiteUrl: '', 
-        bg: 'gradient-2'
-    },{ 
-        id: 107,
-        name: 'Netflix Clone',
-        tag: 'Individual Work',
-        tools: 'Html, CSS and JS',
+        name: "Casa das Plantinhas | Front-end",
+        tag: "Group Work",
+        tools: "Html, Sass, JS, React and React-Bootstrap",
         website: true,
-        gitHubUrl: 'https://github.com/thaysgama/dio-netflix-clone',
-        websiteUrl: 'https://thaysgama.github.io/dio-netflix-clone/',
-        bg: 'gradient-2'
-    },{
+        gitHubUrl: "https://github.com/CheckPointIntegrador/front",
+        websiteUrl: "https://front-red.vercel.app/",
+        bg: "gradient-1",
+      },
+      {
+        id: 102,
+        name: "Casa das Plantinhas | Back-end",
+        tag: "Group Work",
+        tools: "Java and Spring",
+        website: true,
+        gitHubUrl: "https://github.com/CheckPointIntegrador/back",
+        websiteUrl: "https://casadasplantinhas.herokuapp.com/",
+        bg: "gradient-2",
+      },
+      {
+        id: 103,
+        name: "Dentist Registation",
+        tag: "Individual Work",
+        tools: "Java and Spring",
+        website: false,
+        gitHubUrl: "https://github.com/thaysgama/dh-backend-checkpointII",
+        websiteUrl: "",
+        bg: "gradient-3",
+      },
+      {
+        id: 104,
+        name: "To Do List",
+        tag: "Group Work",
+        tools: "Html, CSS and JS",
+        website: true,
+        gitHubUrl: "https://github.com/thaysgama/front-end_II-todo-list",
+        websiteUrl: "https://sophiafiama.github.io/front-end_II-todo-list/",
+        bg: "gradient-2",
+      },
+      {
+        id: 105,
+        name: "Donation Form",
+        tag: "Group Work",
+        tools: "Html, CSS and JS",
+        website: true,
+        gitHubUrl: "https://github.com/thaysgama/dh-frontendII-formulario",
+        websiteUrl: "https://thaysgama.github.io/dh-frontendII-formulario/",
+        bg: "gradient-3",
+      }, 
+      {
+        id: 107,
+        name: "T&M Vehicle Hire",
+        tag: "Pair Work",
+        tools: "Java",
+        website: false,
+        gitHubUrl: "https://github.com/thaysgama/locadora-veiculos",
+        websiteUrl: "",
+        bg: "gradient-1",
+      },
+      {
+        id: 106,
+        name: "Sudaca Filmes",
+        tag: "Group Work",
+        tools: "Html, CSS and JS",
+        website: true,
+        gitHubUrl: "https://github.com/thaysgama/dh-frontend-sudaca",
+        websiteUrl: "https://thaysgama.github.io/dh-frontend-sudaca/",
+        bg: "gradient-1",
+      },
+      {
         id: 108,
-        name: 'Genius Game',
-        tag: 'Individual Work',
-        tools: 'Html, CSS and JS',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/dio-jogo-memoria-genius',
-        websiteUrl: 'https://thaysgama.github.io/dio-jogo-memoria-genius/',
-        bg:'gradient-3'
-    }, {
+        name: "Netflix Clone",
+        tag: "Individual Work",
+        tools: "Html, CSS and JS",
+        website: true,
+        gitHubUrl: "https://github.com/thaysgama/dio-netflix-clone",
+        websiteUrl: "https://thaysgama.github.io/dio-netflix-clone/",
+        bg: "gradient-2",
+      },
+      {
         id: 109,
-        name: 'Landing Page Instagram',
-        tag: 'Individual Work',
-        tools: 'Html and CSS',
-        website:true,
-        gitHubUrl: 'https://github.com/thaysgama/dio-landing-page-instagram',
-        websiteUrl: 'https://thaysgama.github.io/dio-landing-page-instagram/',
-        bg: 'gradient-1'
-    }
-]
+        name: "Genius Game",
+        tag: "Individual Work",
+        tools: "Html, CSS and JS",
+        website: true,
+        gitHubUrl: "https://github.com/thaysgama/dio-jogo-memoria-genius",
+        websiteUrl: "https://thaysgama.github.io/dio-jogo-memoria-genius/",
+        bg: "gradient-3",
+      },
+    ];
 
     return(
         
