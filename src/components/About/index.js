8b2d0459dc49561626b0c5400956e3fc5be0b613
thaysgama <thays.s.gama@gmail.com>
2022-01-19 16:23:19 -0300

    corrige texto sobre nós

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About(){
             <div className="about-text" data-aos="fade-up" data-aos-duration="1500">
                 <h2>About Me</h2>
                 <p>Hi, I’m Thays. Nice to meet you! I’m a Full Stack developer based in Fortaleza, Brazil. I’ve been studying tech since 2020 focusing mostly in Java and Javascript. I am passionate about creating simple, beautiful and intuitive experiences. </p>
-                <p>Before moving into development, I worked designing buildings, as an architect. My experience help me expand my creativity, close attention to detail and team collaboration. I am currently seeking job opportunities to continue growing. Visit my LinkedIn profile for more details or just contact me.</p>
+                <p>Before moving into development, I worked designing buildings, as an architect. My experience helped me expand my creativity, close attention to detail and team collaboration. I am currently seeking job opportunities to continue growing. Visit my LinkedIn profile for more details or just contact me.</p>
                 
                 <div className="icons d-flex flex-wrap align-items-center justify-content-around">
                     <a className='d-flex align-items-center my-4' title="linkedin" href="https://www.linkedin.com/in/thaysgama/"><i class="devicon-linkedin-plain"></i>Linkedin</a>
